eca7ad3b29e11dd440fff50d6746b4b03094c558
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2023-10-05 17:58:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let arr = ["a","b","c","d","e"]
+console.log(arr)
+// - slice (starting index, ending index)
+// ending index not included
+// console.log(arr.slice(2,3))
+//output - ['c']
+console.log(arr.slice(-3,-1))
+// - output = ['c', 'd']
